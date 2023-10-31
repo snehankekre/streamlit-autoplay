55ed2602d33a38f70887cdc2d41f580c4b99fe33
snehankekre <snehan.minerva@gmail.com>
2023-10-31 14:03:17 +0530

    Remove unused toggle

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import numpy as np
 import streamlit as st
 
-st.toggle("Autoplay", True, key="autoplay")
-
-st.video("https://static.streamlit.io/examples/star.mp4", autoplay=st.session_state.autoplay)
+st.video("https://static.streamlit.io/examples/star.mp4", autoplay=True)
 
 sample_rate = 44100  # 44100 samples per second
 seconds = 5  # Note duration of 2 seconds
@@ -13,5 +11,5 @@ t = np.linspace(0, seconds, seconds * sample_rate, False)
 # Generate a 440 Hz sine wave
 note_la = np.sin(frequency_la * t * 2 * np.pi)
 
-st.audio(note_la, sample_rate=sample_rate, autoplay=st.session_state.autoplay)
+st.audio(note_la, sample_rate=sample_rate, autoplay=True)
 
