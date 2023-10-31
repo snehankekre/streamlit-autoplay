d54453c3552498433263c9df07d8c6c044b6dc3c
snehankekre <snehan.minerva@gmail.com>
2023-10-31 13:37:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# POC of autoplaying Streamlit audio and video
+
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://autoplay.streamlit.app)
---
+++ app.py
@@ -0,0 +1,18 @@
+import numpy as np
+import streamlit as st
+
+if st.toggle("Toggle me"):
+    st.write("I'm toggled open")
+
+st.video("https://static.streamlit.io/examples/star.mp4", autoplay=True)
+
+sample_rate = 44100  # 44100 samples per second
+seconds = 5  # Note duration of 2 seconds
+frequency_la = 440  # Our played note will be 440 Hz
+# Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+t = np.linspace(0, seconds, seconds * sample_rate, False)
+# Generate a 440 Hz sine wave
+note_la = np.sin(frequency_la * t * 2 * np.pi)
+
+st.audio(note_la, sample_rate=sample_rate, autoplay=True)
+
